ac27d8f56b233cf40f1ee09eb7b2d8de086f119c
Julia <juliajtan@gmail.com>
2014-07-21 12:05:47 +0100

    Added high level feature specs for map listing page

---
+++ spec/features/race_feature_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe 'Map listing for race' do
+
+  it 'should display a map'
+
+  it 'should show the starting point'
+
+  it 'should show the finish point'
+
+  it 'can show a hotel'
+
+  it 'can show more than one hotel'
+
+end
+
+describe 'Single hotel popup' do
+  it 'shows the name of the hotel'
+
+  it 'shows one picture of the hotel'
+
+  it 'can show more pictures of the hotel'
+
+  it 'shows the hotel rating'
+
+  it 'shows the hotel price per night'
+
+  it 'hotel which has a gym'
+
+  it 'hotel which has no gym'
+
+  it 'shows the distance from the starting point of the race'
+
+  it 'shows the walking time from the starting point'
+
+  it 'shows a suggestion on when to leave the hotel'
+
+end
