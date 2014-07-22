1777bb7091db75f2011829aa0b4ada540622ec38
Julia <juliajtan@gmail.com>
2014-07-22 15:54:09 +0100

    Used jsonbuilder to take Expedia API and convert it to customised JSON file from within the controller

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,3 +1,15 @@
+# require 'json'
+# require 'open-uri'
+
 class HotelsController < ApplicationController
+  def index
+
+    url = "https://api.eancdn.com/ean-services/rs/hotel/v3/list?cid=464671&minorRev=99&apiKey=rthjbmnexf9e6z7863ru5w9n&locale=en_US&currencyCode=GBP&latitude=52.5167&longitude=13.3833&arrivalDate=08%2F01%2F14&departureDate=08%2F20%2F14&numberOfResults=10&minStarRating=4&searchRadius=5&searchRadiusUnit=KM&sort=PROXIMITY"
   
+    data = open(url).read
+
+    @hotels = JSON.parse(data)["HotelListResponse"]["HotelList"]["HotelSummary"]
+
+    # render json: @index 
+  end
 end
