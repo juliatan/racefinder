c9a69b02723506fe3557ccc2315466becd17b438
Julia <juliajtan@gmail.com>
2014-07-22 23:54:34 +0100

    Integrated Expedia API into races javascript file so that hotel markers are generated dynamically

--- app/assets/javascripts/races.js
+++ app/assets/javascripts/races.js
@@ -1,74 +1,77 @@
 $(document).ready(function(){
 
-  var hotels = [
-    {
-      name: 'The Ritz Carlton',
-      address: 'Postdamer Platz 3, 10785 Berlin, Germany',
-      lat: 52.51003,
-      lng: 13.375434,
-      rating: '4.3',
-      price: '£205',
-      gym: 'Gym ✓',
-      wifi: 'Free Wifi ✓',
-      breakfast: 'Breakfast not included ✗',
-      photo1: 'ritz_carlton_1.jpg'
-    },
-    {
-      name: 'The Mandala Hotel',
-      address: 'Postdamer Straße 3, 10785 Berlin, Germany',
-      lat: 52.5093,
-      lng: 13.3737,
-      rating: '4.5',
-      price: '£133',
-      gym: 'Gym ✓',
-      wifi: 'Free Wifi ✓',
-      breakfast: 'Breakfast included ✓',
-      photo1: 'mandala_hotel_1.jpg'
-    }
-  ];
+  $.get('/hotels.json', function(json){
+    console.log(json);
+    var hotels = json;
+  // var hotels = [
+  //   {
+  //     name: 'The Ritz Carlton',
+  //     address: 'Postdamer Platz 3, 10785 Berlin, Germany',
+  //     lat: 52.51003,
+  //     lng: 13.375434,
+  //     rating: '4.3',
+  //     price: '£205',
+  //     gym: 'Gym ✓',
+  //     wifi: 'Free Wifi ✓',
+  //     breakfast: 'Breakfast not included ✗',
+  //     photo1: 'ritz_carlton_1.jpg'
+  //   },
+  //   {
+  //     name: 'The Mandala Hotel',
+  //     address: 'Postdamer Straße 3, 10785 Berlin, Germany',
+  //     lat: 52.5093,
+  //     lng: 13.3737,
+  //     rating: '4.5',
+  //     price: '£133',
+  //     gym: 'Gym ✓',
+  //     wifi: 'Free Wifi ✓',
+  //     breakfast: 'Breakfast included ✓',
+  //     photo1: 'mandala_hotel_1.jpg'
+  //   }
+  // ];
 
-  var map = new GMaps({
-    div: '#map',
-    lat: 0,
-    lng: 0
-  });
+    var map = new GMaps({
+      div: '#map',
+      lat: 0,
+      lng: 0
+    });
 
-  var iconOffset = 0.0015
+    var iconOffset = 0.0015
 
-  GMaps.geocode({
-    address: 'Straße des 17. Juni 31, Berlin, Germany',
-    callback: function(results, status) {
-      if (status == 'OK') {
-        var latlng = results[0].geometry.location;
-        map.setCenter(latlng.lat(), latlng.lng());
-        map.addMarker({
-          lat: 52.51518 - iconOffset,
-          lng: 13.35938,
-          icon: "/start_line_icon.png"
-        });
+    GMaps.geocode({
+      address: 'Straße des 17. Juni 31, Berlin, Germany',
+      callback: function(results, status) {
+        if (status == 'OK') {
+          var latlng = results[0].geometry.location;
+          map.setCenter(latlng.lat(), latlng.lng());
+          map.addMarker({
+            lat: 52.51518 - iconOffset,
+            lng: 13.35938,
+            icon: "/start_line_icon.png"
+          });
+        }
       }
-    }
-  });
+    });
 
-  map.addMarker({
-    lat: 52.51622 - iconOffset,
-    lng: 13.37573,
-    title: 'Finish Line',
-    icon: "/finish_line_icon.png",
+    map.addMarker({
+      lat: 52.51622 - iconOffset,
+      lng: 13.37573,
+      title: 'Finish Line',
+      icon: "/finish_line_icon.png",
 
-    // click: function(e) {
-      // alert('You clicked in this marker');
-    // }
-    infoWindow: {
-      content: '<p>Finish Line</p>'
-    }
-  });
+      // click: function(e) {
+        // alert('You clicked in this marker');
+      // }
+      infoWindow: {
+        content: '<p>Finish Line</p>'
+      }
+    });
 
-  hotels.forEach(function(hotel){
-    var route = map.getRoutes({
-      origin: [hotel.lat, hotel.lng], //hotel coordinates
-      destination: [52.51518, 13.35938], //start line coordinates
-      callback: function (e) {
+    hotels.forEach(function(hotel){
+      var route = map.getRoutes({
+        origin: [hotel.lat, hotel.long], //hotel coordinates
+        destination: [52.51518, 13.35938], //start line coordinates
+        callback: function (e) {
           var time = 0;
           var distance = 0;
           var legs = e.pop().legs;
@@ -88,21 +91,19 @@ $(document).ready(function(){
 
           map.addMarker({
             lat: hotel.lat,
-            lng: hotel.lng,
-            title: 'hotel',
+            lng: hotel.long,
+            title: hotel.name,
             icon: "/hotel_icon.png",
             infoWindow: {
               content: 
-                '<p><img class="hotel-photo" src="/assets/' + hotel.photo1 + '"></p><p>'+ hotel.name + '</p><p>' + hotel.address + '</p><p>' + hotel.rating + '</p><p>'+ hotel.price + '</p><p>' + hotel.gym + '</p><p>' + hotel.breakfast + '</p><p>' + hotel.wifi + '</p><p>'
+                '<p><img class="hotel-photo" src="/assets/' + hotel.photo1 + '"></p><p>'+ hotel.name + '</p><p>' + hotel.address + '</p><p>Rating: ' + hotel.rating + '</p><p>Price per night: £'+ hotel.price + '</p><p>' + hotel.gym + '</p><p>' + hotel.breakfast + '</p><p>' + hotel.wifi + '</p><p>'
                 + hotel.minutes + ' minutes walk from start line (' + hotel.km + ' km)</p>'
             }
           });
-      }
-    });
+        }
+      });
+    })
+
+    window.map = map;
   })
-
-
-
-  window.map = map;
-
 })
--- app/views/hotels/index.json.jbuilder
+++ app/views/hotels/index.json.jbuilder
@@ -1,9 +1,10 @@
 json.array! @hotels do |hotel|
   json.name hotel["name"]
+  json.address hotel["address1"]
   json.id hotel["hotelID"]
   json.lat hotel["latitude"]
   json.long hotel["longitude"]
   json.price hotel["RoomRateDetailsList"]["RoomRateDetails"]["RateInfos"]["RateInfo"]["ChargeableRateInfo"]["@averageRate"]
-  json.hotelRating hotel["hotelRating"]
+  json.rating hotel["hotelRating"]
   json.tripAdvisorRating hotel["tripAdvisorRating"]
 end
