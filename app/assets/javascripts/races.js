17646700289aa42f521c482e1fe1daf3a0cf135c
Nic Yeeles <n.yeeles@gmail.com>
2014-07-23 11:27:45 +0100

    Only load javascript if a map is present - fixes starting point test

--- app/assets/javascripts/races.js
+++ app/assets/javascripts/races.js
@@ -1,116 +1,116 @@
 $(document).ready(function(){
-
-  
-  var hotels = [
-    {
-      name: 'The Ritz Carlton',
-      address: 'Postdamer Platz 3, 10785 Berlin, Germany',
-      lat: 52.51003,
-      lng: 13.375434,
-      rating: '4.3',
-      price: '£205',
-      gym: 'Gym ✓',
-      wifi: 'Free Wifi ✓',
-      breakfast: 'Breakfast not included ✗',
-      photo1: 'ritz_carlton_1.jpg'
-    },
-    {
-      name: 'The Mandala Hotel',
-      address: 'Postdamer Straße 3, 10785 Berlin, Germany',
-      lat: 52.5093,
-      lng: 13.3737,
-      rating: '4.5',
-      price: '£133',
-      gym: 'Gym ✓',
-      wifi: 'Free Wifi ✓',
-      breakfast: 'Breakfast included ✓',
-      photo1: 'mandala_hotel_1.jpg'
-    }
-  ];
-
-  var map = new GMaps({
-    div: '#map',
-    lat: 0,
-    lng: 0
-  });
-
-  var iconOffset = 0.0015
-  
-  function locationPreference() {
-    if ($("#map").data('race') == 'Start line') {
-      return [52.51518, 13.35938]
-    } else {
-      return [52.51622, 13.37573]
-    };
-  };
-    
-  GMaps.geocode({
-    lat: locationPreference()[0],
-    lng: locationPreference()[1],
-    callback: function(results, status) {
-      if (status == 'OK') {
-        var latlng = results[0].geometry.location;
-        map.setCenter(latlng.lat(), latlng.lng());
-        map.addMarker({
-          lat: 52.51518 - iconOffset,
-          lng: 13.35938,
-          icon: "/start_line_icon.png"
-        });
+  if($('#map').length > 0) {
+    var hotels = [
+      {
+        name: 'The Ritz Carlton',
+        address: 'Postdamer Platz 3, 10785 Berlin, Germany',
+        lat: 52.51003,
+        lng: 13.375434,
+        rating: '4.3',
+        price: '£205',
+        gym: 'Gym ✓',
+        wifi: 'Free Wifi ✓',
+        breakfast: 'Breakfast not included ✗',
+        photo1: 'ritz_carlton_1.jpg'
+      },
+      {
+        name: 'The Mandala Hotel',
+        address: 'Postdamer Straße 3, 10785 Berlin, Germany',
+        lat: 52.5093,
+        lng: 13.3737,
+        rating: '4.5',
+        price: '£133',
+        gym: 'Gym ✓',
+        wifi: 'Free Wifi ✓',
+        breakfast: 'Breakfast included ✓',
+        photo1: 'mandala_hotel_1.jpg'
       }
-    }
-  });
+    ];
 
-  map.addMarker({
-    lat: 52.51622 - iconOffset,
-    lng: 13.37573,
-    title: 'Finish Line',
-    icon: "/finish_line_icon.png",
+    var map = new GMaps({
+      div: '#map',
+      lat: 0,
+      lng: 0
+    });
 
-    // click: function(e) {
-      // alert('You clicked in this marker');
-    // }
-    infoWindow: {
-      content: '<p>Finish Line</p>'
-    }
-  });
-
-  hotels.forEach(function(hotel){
-    var route = map.getRoutes({
-      origin: [hotel.lat, hotel.lng], //hotel coordinates
-      destination: [52.51518, 13.35938], //start line coordinates
-      callback: function (e) {
-          var time = 0;
-          var distance = 0;
-          var legs = e.pop().legs;
-
-          for (var i=0; i<legs.length; i++) {
-              time += legs[i].duration.value;
-              distance += legs[i].distance.value;
-          }
-          var minutesH = time/60;
-          var minutes = Math.round(minutesH*100)/100;
-          
-          var metres = distance/1000;
-          var km = Math.round(metres*100)/100;
-
-          hotel.minutes = minutes;
-          hotel.km = km;
-
+    var iconOffset = 0.0015
+    
+    function locationPreference() {
+      if ($("#map").data('preferred_location') == 'Start line') {
+        return [52.51518, 13.35938]
+      } else {
+        return [52.51622, 13.37573]
+      };
+    };
+      
+    GMaps.geocode({
+      lat: locationPreference()[0],
+      lng: locationPreference()[1],
+      callback: function(results, status) {
+        if (status == 'OK') {
+          var latlng = results[0].geometry.location;
+          map.setCenter(latlng.lat(), latlng.lng());
           map.addMarker({
-            lat: hotel.lat,
-            lng: hotel.lng,
-            title: 'hotel',
-            icon: "/hotel_icon.png",
-            infoWindow: {
-              content: 
-                '<p><img class="hotel-photo" src="/assets/' + hotel.photo1 + '"></p><p>'+ hotel.name + '</p><p>' + hotel.address + '</p><p>' + hotel.rating + '</p><p>'+ hotel.price + '</p><p>' + hotel.gym + '</p><p>' + hotel.breakfast + '</p><p>' + hotel.wifi + '</p><p>'
-                + hotel.minutes + ' minutes walk from start line (' + hotel.km + ' km)</p>'
-            }
+            lat: 52.51518 - iconOffset,
+            lng: 13.35938,
+            icon: "/start_line_icon.png"
           });
+        }
       }
     });
-  })
+
+    map.addMarker({
+      lat: 52.51622 - iconOffset,
+      lng: 13.37573,
+      title: 'Finish Line',
+      icon: "/finish_line_icon.png",
+
+      // click: function(e) {
+        // alert('You clicked in this marker');
+      // }
+      infoWindow: {
+        content: '<p>Finish Line</p>'
+      }
+    });
+
+    hotels.forEach(function(hotel){
+      var route = map.getRoutes({
+        origin: [hotel.lat, hotel.lng], //hotel coordinates
+        destination: [52.51518, 13.35938], //start line coordinates
+        callback: function (e) {
+            var time = 0;
+            var distance = 0;
+            var legs = e.pop().legs;
+
+            for (var i=0; i<legs.length; i++) {
+                time += legs[i].duration.value;
+                distance += legs[i].distance.value;
+            }
+            var minutesH = time/60;
+            var minutes = Math.round(minutesH*100)/100;
+            
+            var metres = distance/1000;
+            var km = Math.round(metres*100)/100;
+
+            hotel.minutes = minutes;
+            hotel.km = km;
+
+            map.addMarker({
+              lat: hotel.lat,
+              lng: hotel.lng,
+              title: 'hotel',
+              icon: "/hotel_icon.png",
+              infoWindow: {
+                content: 
+                  '<p><img class="hotel-photo" src="/assets/' + hotel.photo1 + '"></p><p>'+ hotel.name + '</p><p>' + hotel.address + '</p><p>' + hotel.rating + '</p><p>'+ hotel.price + '</p><p>' + hotel.gym + '</p><p>' + hotel.breakfast + '</p><p>' + hotel.wifi + '</p><p>'
+                  + hotel.minutes + ' minutes walk from start line (' + hotel.km + ' km)</p>'
+              }
+            });
+        }
+      });
+    })
 
 
-  window.map = map;
+    window.map = map;
+  }
 })
--- spec/features/homepage_feature_spec.rb
+++ spec/features/homepage_feature_spec.rb
@@ -5,19 +5,11 @@ describe 'Homepage' do
 
   it 'can select more than one marathon from a dropdown menu'
 
-  it 'can select starting point as reference' do
+  it 'can select starting point as reference', js:true do
   	race = Race.create(name: "Berlin Marathon", address: 'Straße des 17. Juni 31, Berlin, Germany')
   	visit '/'
-  	select 'Start line', from: 'Preferred location'
+  	select 'Start line', from: 'preferred_location'
   	click_button 'Search'
-
-  	map_center_lng = page.evaluate_script("map.getCenter().lng()")
-  	map_center_lat = page.evaluate_script("map.getCenter().lat()")
-
-  	expect(map_center_lat).to be_within(0.05).of 13.363679999999922
-  	expect(map_center_lng).to be_within(0.05).of 52.51414999999994
-
-  	visit '/races'
   	sleep 1
   	expect(page.evaluate_script("map.getCenter().lng()")).to be_within(0.05).of 13.363679999999922
   	expect(page.evaluate_script("map.getCenter().lat()")).to be_within(0.05).of 52.51414999999994
