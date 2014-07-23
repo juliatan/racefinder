24101090104ff7b5b7e4c7ba678c3b17dbd59e28
Julia <juliajtan@gmail.com>
2014-07-23 16:01:06 +0100

    Current date interpolates correctly for API call

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -4,15 +4,24 @@ require 'open-uri'
 class HotelsController < ApplicationController
   def index
 
-    # current_date = Time.now.strftime('%m/%d/%y').gsub('/', '%2F')
-    current_date = Time.now.strftime('%m/%d/%Y')
-    departure_date = current_date + params[:nights]
+    # @current_date = Time.now
+    # @current_date_formatted = @current_date.strftime('%m/%d/%y').gsub('/',"%2F")
+    # @departure_date = @current_date + params[:nights].to_i*24*60*60
+    # @departure_date_formatted = @departure_date.strftime('%m/%d/%y').gsub('/',"%2F")
+
+    # @current_date_formatted = "07%2F23%2F14"
+    # @departure_date_formatted = "07%2F30%2F14"
+
+    @current_date = Time.now
+    @current_date_formatted = "#{@current_date.strftime('%m/%d/%y').gsub('/',"%2F")}"
+    @departure_date = @current_date + params[:nights].to_i*24*60*60
+    @departure_date_formatted = "#{@departure_date.strftime('%m/%d/%y').gsub('/',"%2F")}"
 
     # url = "https://api.eancdn.com/ean-services/rs/hotel/v3/list?cid=464671&minorRev=99&apiKey=rthjbmnexf9e6z7863ru5w9n&locale=en_US&currencyCode=GBP&latitude=52.5167&longitude=13.3833&arrivalDate=08%2F01%2F14&departureDate=08%2F20%2F14&numberOfResults=10&minStarRating=4&searchRadius=5&searchRadiusUnit=KM&sort=PROXIMITY&maxRate=300"
 
-    url = "https://api.eancdn.com/ean-services/rs/hotel/v3/list?cid=464671&minorRev=99&apiKey=rthjbmnexf9e6z7863ru5w9n&locale=en_US&currencyCode=GBP&latitude=52.5167&longitude=13.3833&arrivalDate=" + current_date + "&departureDate=" + departure_date + "&numberOfResults=10&minStarRating=4&searchRadius=5&searchRadiusUnit=KM&sort=PROXIMITY&maxRate=300"
+    @url = "https://api.eancdn.com/ean-services/rs/hotel/v3/list?cid=464671&minorRev=99&apiKey=rthjbmnexf9e6z7863ru5w9n&locale=en_US&currencyCode=GBP&latitude=52.5167&longitude=13.3833&arrivalDate=" + @current_date_formatted + "&departureDate=" + @departure_date_formatted + "&numberOfResults=10&minStarRating=4&searchRadius=5&searchRadiusUnit=KM&sort=PROXIMITY&maxRate=300"
   
-    data = open(url).read
+    data = open(@url).read
 
     @hotels = JSON.parse(data)["HotelListResponse"]["HotelList"]["HotelSummary"]
 
