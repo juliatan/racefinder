c87e683f3cfbbb07289d24e04bd995e9b9738e13
Julia <juliajtan@gmail.com>
2014-07-24 11:28:34 +0100

    Added city-marathon dropdown menu to homepage

--- spec/features/homepage_feature_spec.rb
+++ spec/features/homepage_feature_spec.rb
@@ -1,13 +1,34 @@
 require 'rails_helper'
 
 describe 'Homepage' do
-  it 'can select one marathon'
+  it 'can select Berlin', js:true do
+    visit '/'
+    select 'Berlin', from: 'marathon'
+    select 'Start line', from: 'preferred_location'
+    fill_in 'Number of nights', with: '1'
+    click_button 'Search'
+    sleep 1
+    expect(page.evaluate_script("map.getCenter().lng()")).to be_within(0.05).of 13.363679999999922
+    expect(page.evaluate_script("map.getCenter().lat()")).to be_within(0.05).of 52.51414999999994
+  end
+
+  it 'can select London', js:true do
+    visit '/'
+    select 'London', from: 'marathon'
+    select 'Start line', from: 'preferred_location'
+    fill_in 'Number of nights', with: '1'
+    click_button 'Search'
+    sleep 1
+    expect(page.evaluate_script("map.getCenter().lng()")).to be_within(0.05).of -0.001416
+    expect(page.evaluate_script("map.getCenter().lat()")).to be_within(0.05).of 51.477805
+  end
 
   it 'can select more than one marathon from a dropdown menu'
 
   it 'can select starting point as reference', js:true do
   	race = Race.create(name: "Berlin Marathon", address: 'Straße des 17. Juni 31, Berlin, Germany')
   	visit '/'
+    select 'Berlin', from: 'marathon'
   	select 'Start line', from: 'preferred_location'
     fill_in 'Number of nights', with: '1'
   	click_button 'Search'
@@ -21,11 +42,9 @@ describe 'Homepage' do
   it 'can select one budget range from dropdown menu'
 
   context 'number of nights' do
-    xit 'displays one night in the dropdown' do
-      visit '/'
-      select '1 night', from: 'nights'
-      click_button 'Search'
-      expect(current_path).to eq races_path
+    it 'user can fill in number of nights' do
+      enter_race_details
+      expect(current_url).to eq 'http://www.example.com/races?utf8=%E2%9C%93&preferred_location=Start+line&nights=1&commit=Search'
     end
   end
 
--- spec/support/race_helper.rb
+++ spec/support/race_helper.rb
@@ -1,6 +1,7 @@
 def enter_race_details
   visit '/'
+  select 'Berlin', from: 'marathon'
   select 'Start line', from: 'preferred_location'
-  fill_in 'Number of nights', with: '1'
+  fill_in 'nights', with: '1'
   click_button 'Search'
 end
