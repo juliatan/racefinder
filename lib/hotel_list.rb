47f83dd6ed8b80abd0eb199d7a70956c5f8653f6
Julia <juliajtan@gmail.com>
2014-07-24 14:53:50 +0100

    Created raketask for populating hotel database

---
+++ lib/hotel_list.rb
@@ -0,0 +1,35 @@
+def hotel_api_call
+
+  url = "https://api.eancdn.com/ean-services/rs/hotel/v3/list?cid=464671&minorRev=99&apiKey=rthjbmnexf9e6z7863ru5w9n&locale=en_US&currencyCode=GBP&latitude=52.5167&longitude=13.3833&numberOfResults=10&minStarRating=4&searchRadius=5&searchRadiusUnit=KM&sort=PROXIMITY&maxRate=300"
+  
+  data = open(url).read
+  hotels = JSON.parse(data)["HotelListResponse"]["HotelList"]["HotelSummary"]
+
+  hotels.each do |hotel|
+    hotel_id = hotel["hotelId"]
+    name = hotel["name"]
+    address = hotel["address1"]
+    lat = hotel["latitude"]
+    long = hotel["longitude"]
+    price = hotel["RoomRateDetailsList"]["RoomRateDetails"]["RateInfos"]["RateInfo"]["ChargeableRateInfo"]["@averageRate"]
+    rating = hotel["hotelRating"]
+    tripadvisor_rating = hotel["tripAdvisorRating"]
+
+    Hotel.create(hotel_id: hotel_id, name: name, address: address, lat: lat, long: long, price: price, rating: rating, tripadvisor_rating: tripadvisor_rating)
+  end
+
+end
+
+# def hotel_images
+# json.images @hotels_info[index]["HotelImages"]["HotelImage"].first(4) do |image|
+#     json.image_url image["url"]
+#   end
+
+# end
+
+# current_date = Time.now
+#     current_date_formatted = "#{current_date.strftime('%m/%d/%y').gsub('/',"%2F")}"
+#     departure_date = current_date + params[:nights].to_i*24*60*60
+#     departure_date_formatted = "#{departure_date.strftime('%m/%d/%y').gsub('/',"%2F")}"
+
+  
---
+++ lib/tasks/populate_hotel_table.rake
@@ -0,0 +1,5 @@
+require 'hotel_list'
+
+task :populate_hotel_table do
+  hotel_api_call
+end
