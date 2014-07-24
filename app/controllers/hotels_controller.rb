6fcb6491eb4af587178d3ae6f3ce0d90980e9a33
Margherita <m.serino@hotmail.it>
2014-07-24 17:38:20 +0100

    Calendar feature - user can choose arrival and departure date and it calculates number of night basing on the user choices - not styled yet

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -17,18 +17,10 @@ class HotelsController < ApplicationController
       departure_y = d.join
     arrival_formatted = "#{arrival_y.gsub('/', '%2F')}"
     departure_formatted = "#{departure_y.gsub('/', '%2F')}"
-    # arrival11 = Date.strptime(arrival, '%m/%d/%Y')
-    # departure11 = Date.strptime(departure, '%m/%d/%Y')
-    # number_of_nights = (departure11 - arrival11).to_i
-
-    # current_date = Time.now
-    # current_date_formatted = "#{current_date.strftime('%m/%d/%y').gsub('/',"%2F")}"
-    # departure_date = current_date + params[:nights].to_i*24*60*60
-    # departure_date_formatted = "#{departure_date.strftime('%m/%d/%y').gsub('/',"%2F")}"
-
-    @url = "https://api.eancdn.com/ean-services/rs/hotel/v3/list?cid=464671&minorRev=99&apiKey=rthjbmnexf9e6z7863ru5w9n&locale=en_US&currencyCode=GBP&latitude=52.5167&longitude=13.3833&arrivalDate=" + arrival_formatted + "&departureDate=" + departure_formatted + "&numberOfResults=10&minStarRating=4&searchRadius=5&searchRadiusUnit=KM&sort=PROXIMITY&maxRate=300"
+    
+    url = "https://api.eancdn.com/ean-services/rs/hotel/v3/list?cid=464671&minorRev=99&apiKey=rthjbmnexf9e6z7863ru5w9n&locale=en_US&currencyCode=GBP&latitude=52.5167&longitude=13.3833&arrivalDate=" + arrival_formatted + "&departureDate=" + departure_formatted + "&numberOfResults=10&minStarRating=4&searchRadius=5&searchRadiusUnit=KM&sort=PROXIMITY&maxRate=300"
   
-    data = open(@url).read
+    data = open(url).read
 
     @hotels = JSON.parse(data)["HotelListResponse"]["HotelList"]["HotelSummary"]
 
