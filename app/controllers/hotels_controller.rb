d721e136dbb61f692ea58328594627daf329691c
Julia <juliajtan@gmail.com>
2014-07-26 11:25:27 +0100

    Budget feature bug fixed for params price in race index page

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -11,9 +11,7 @@ class HotelsController < ApplicationController
 
     @city_hotels = Hotel.where(race_id: @race_id)
 
-    # @city_hotels_within_budget = @city_hotels.where("price < ?", params[:price].to_f)
-    # Client.first(:conditions => ["orders_count = ?", params[:orders]])
-    @city_hotels_within_budget = @city_hotels.where("price > ?", params[:price].to_f)
+    @city_hotels_within_budget = @city_hotels.where("price < ?", params[:price])
     
     @city_hotels_selected = @city_hotels_within_budget.where(ref_lat: @ref_lat)
   end
