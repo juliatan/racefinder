b63b36e3a9a582bc169b3f02a58b9211f8edfafe
Julia <juliajtan@gmail.com>
2014-07-28 20:09:03 +0100

    Auto set departure date based on marathon choice

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
   $('.input-daterange').datepicker({
     todayHighlight: true,
     // format: "dd M yy",
-    startDate: 'today'
+    startDate: 'today',
   });
 
   // extract to database?
@@ -29,13 +29,23 @@ $(document).ready(function(){
   $('#marathon').on('change', function(){
     var raceSelection = $('#marathon option:selected').text();
     var startDate = new Date(marathonStartDates[raceSelection]); // creates Date object from string
-    var arrivalDateObject = (new Date(startDate - 1000*60*60*24)); // default is one day prior to marathon start date
+    
+    var arrivalDateObject = new Date(startDate.getTime() - 1000*60*60*24); // default is one day prior to marathon start date
     var arrivalDate = arrivalDateObject.getDate();
     var arrivalMonth = arrivalDateObject.getMonth() + 1; // .getMonth() method starts from Jan = 0
     var arrivalYear = arrivalDateObject.getFullYear();
     var arrivalString = arrivalMonth + '/' + arrivalDate + '/' + arrivalYear;
+
+    var departureDateObject = new Date(startDate.getTime() + 1000*60*60*24); // default is one day after the marathon start date
+    var departureDate = departureDateObject.getDate();
+    var departureMonth = departureDateObject.getMonth() + 1;
+    var departureYear = departureDateObject.getFullYear();
+    var departureString = departureMonth + '/' + departureDate + '/' + departureYear;
+
     // This code also works: var arrivalDate = new Date(startDate.setDate(startDate.getDate()-1));
     $('.input-daterange').datepicker().children('input.arrival').val(arrivalString); // amend value of arrival date automatically
+
+    $('.input-daterange').datepicker().children('input.departure').val(departureString); // amend value of departure date automatically
     
     // $('.input-daterange').attr('data-date', startDate) - this might be the code used to amend calender?
   });
