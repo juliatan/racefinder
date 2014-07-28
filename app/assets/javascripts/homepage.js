23dd4a446c645abc9d75d52aaea5e65deeab15ba
Julia <juliajtan@gmail.com>
2014-07-28 17:15:20 +0100

    Fix carousel for Heroku

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -36,8 +36,34 @@ function CarouselCtrl($scope) {
 $(document).ready(function(){
   // datepicker rails
   $('.input-daterange').datepicker({
-    todayHighlight: true
+    todayHighlight: true,
+    // format: "dd M yy",
+    startDate: 'today'
   });
+
+  // extract to database?
+  var marathonStartDates = {
+    'Berlin': '2014-09-28',
+    'London': '2015-04-26',
+    'Paris': '2015-04-12',
+    'Stockholm': '2015-05-30',
+    'Sydney': '2014-09-21'
+  };
+
+  $('#marathon').on('change', function(){
+    var raceSelection = $('#marathon option:selected').text();
+    var startDate = new Date(marathonStartDates[raceSelection]); // creates Date object from string
+    var arrivalDateObject = (new Date(startDate - 1000*60*60*24)); // default is one day prior to marathon start date
+    var arrivalDate = arrivalDateObject.getDate();
+    var arrivalMonth = arrivalDateObject.getMonth() + 1; // .getMonth() method starts from Jan = 0
+    var arrivalYear = arrivalDateObject.getFullYear();
+    var arrivalString = arrivalMonth + '/' + arrivalDate + '/' + arrivalYear;
+    // This code also works: var arrivalDate = new Date(startDate.setDate(startDate.getDate()-1));
+    $('.input-daterange').datepicker().children('input.arrival').val(arrivalString); // amend value of arrival date automatically
+    
+    // $('.input-daterange').attr('data-date', startDate) - this might be the code used to amend calender?
+  });
+
 
   // to add £ sign to max budget dropdown menu
   $('.price option').slice(1,4).prepend('£');
