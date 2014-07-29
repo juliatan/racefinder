4873566bfde594eb507e60a1fd533ae14288e0f6
Margherita <m.serino@hotmail.it>
2014-07-29 13:51:47 +0100

    User can sign up and sign in, can add the name which is displayed in the homepage

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -4,12 +4,13 @@ describe 'user registration/login' do
 	it 'can sign up' do
 		visit '/users/sign_up'
 		within '.new_user' do
+			fill_in 'Name', with: 'Maggie'
 			fill_in 'Email', with: 'test@example.com'
 			fill_in 'Password', with: '12345678'
 			fill_in 'Password confirmation', with: '12345678'
-			fill_in 'Phone number', with: '+447404992798'
+			# fill_in 'Phone number', with: '+447404992798'
 			click_button 'Sign up'
 		end
-		expect(page).to have_content 'Signed up successfully'
+		expect(page).to have_content 'Where will you conquer, Maggie?'
 	end
 end
