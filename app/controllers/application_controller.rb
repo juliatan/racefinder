e3eae4ac86259de98be474422d4b78a554a8f82b
Nic Yeeles <n.yeeles@gmail.com>
2014-07-29 15:46:20 +0100

    User can sign up with a phone number

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
   	devise_parameter_sanitizer.for(:sign_up) << :name
+  	devise_parameter_sanitizer.for(:sign_up) << :phone_number
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140729104237) do
+ActiveRecord::Schema.define(version: 20140729144302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20140729104237) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "name"
+    t.text     "phone_number"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
