50312f92a5f3c7e5406689519e1ace20a95b0683
Margherita <m.serino@hotmail.it>
2014-07-30 22:52:04 +0100

    Styled email for user itinerary

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,11 +1,12 @@
 class Mailer < ActionMailer::Base
   default from: "no-reply@strides.com"
 
-  def confirmation(hotel, user, arrival, departure)
+  def confirmation(hotel, user, arrival, departure, marathon)
   	@user = user
     @hotel = hotel
     @arrival = arrival
     @departure = departure
+    @marathon = marathon
   	mail(to: @user.email, subject: '*Strides* Your itinerary')
   end
 end
